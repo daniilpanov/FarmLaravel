4228146f41b2b0aab52bfa42a9a8f4884cfa531c
Daniil Panov <daniilp@amazingcat.net>
2022-11-07 11:58:40 +0300

    it's only beginning...

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import { createApp, h } from 'vue/dist/vue.esm-bundler.js';
+import { createInertiaApp, InertiaLink } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+
+InertiaProgress.init();
+
+createInertiaApp({
+    id: 'app',
+    resolve: name => import(`./Pages/${name}.vue`),
+    setup({ el, App, props, plugin }) {
+        createApp({
+            render: () => h(App, props),
+        }).use(plugin).component('inertia-link', InertiaLink).mount(el);
+    },
+})
